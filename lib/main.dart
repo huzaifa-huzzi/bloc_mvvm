f7dc78eb6801a6c1c8713420398b2108c60ac526
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-24 16:02:05 +0500

    1.Making bloc Mvvm

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_mvvm/views/splash/splashScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'bloc Mvvm',
-      home:
+      home:Splashscreen(),
     );
   }
 }

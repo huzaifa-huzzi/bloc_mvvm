b474188d1c357e2b9c5d28d716dcb9c4ea0da02c
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-24 16:10:11 +0500

    1.Making bloc Mvvm

---
+++ lib/config/Routes/Routes_Initializing.dart
@@ -0,0 +1,36 @@
+
+import 'package:bloc_mvvm/config/Routes/Routes_name.dart';
+import 'package:bloc_mvvm/views/home/home_screen.daart.dart';
+import 'package:bloc_mvvm/views/login/LoginScreen.dart';
+import 'package:bloc_mvvm/views/splash/splashScreen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+  
+  static Route<dynamic> generateRoute(RouteSettings settings){
+    switch(settings.name){
+
+      case RoutesName.splashScreen:
+        return MaterialPageRoute(builder: (context) =>const  Splashscreen());
+
+      case RoutesName.homeScreen:
+        return MaterialPageRoute(builder: (context) =>const  HomeScreen());
+
+      case RoutesName.loginScreen:
+        return MaterialPageRoute(builder: (context) =>const  LoginScreen());
+
+      default :
+        return MaterialPageRoute(builder: (context){
+          return const Scaffold(
+            body: Center(
+              child: Text('No Page Generate'),
+            ),
+          );
+        });
+    }
+    
+    
+  }
+  
+}
